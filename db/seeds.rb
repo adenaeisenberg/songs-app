ba2a750594414e68bafd245339b347498b1aada3
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-07-10 15:05:49 -0400

    added songs to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+song = Song.create(title: "Happy Birthday", album: "Most Common Songs", artist: "Mildred and Patty Smith Hill", year: 1893)
+
+song = Song.create(title: "ABCs", album: "Most Common Songs", artist: "Charles Bradlee", year: 1834)
+
+song = Song.create(title: "Twinkle Twinkle Little Star", album: "Most Common Songs", artist: "Jane Taylor", year: 1806)
