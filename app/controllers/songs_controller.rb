a1de23ceb49e451560ace1b97c00ecffe9b256ce
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-11-21 16:36:45 -0500

    UTD

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,20 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render :index
+  end
+
+  def create
+    @song = Song.create(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    render :show
+  end
+
+  def show
+    @song = Song.find_by
+  end
 end
